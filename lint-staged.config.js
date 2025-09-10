40b51cd962bfa849dab5a2e5ea99dc3c773ab368
DevasNB <joaodevesa2019@gmail.com>
2025-09-10 23:33:11 +0100

    chore: added global husky configuration

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+export default {
+    "frontend/**/*.{ts,js,html,css,scss}": [
+        "prettier --write",
+        "cd frontend && npm run lint --if-present"
+    ],
+    "backend/**/*.{ts,js,json,prisma}": [
+        "prettier --write",
+        "cd backend && npm run lint --if-present"
+    ]
+}
