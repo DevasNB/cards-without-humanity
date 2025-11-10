d44909e67086c79b90b823af38f7095fb3d4667f
Bruno Carvalho <61797027+BrunixD@users.noreply.github.com>
2025-11-10 19:05:44 +0000

    feat: Updates route with new pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { CreateLobbyComponent } from './features/lobby/create-lobby/create-lobby';
+import { JoinLobbyComponent } from './features/lobby/join-lobby/join-lobby';
+import { ListDecksComponent } from './features/decks/list-decks/list-decks';
+import { CreateDeckComponent } from './features/decks/create-deck/create-deck';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
+
+  // Lobby Routes
+  { path: 'lobby/create-lobby', component: CreateLobbyComponent },
+  { path: 'lobby/join-lobby', component: JoinLobbyComponent },
+
+  // Decks Routes
+  { path: 'decks/list-decks', component: ListDecksComponent },
+  { path: 'decks/create-deck', component: CreateDeckComponent },
+
   { path: '**', redirectTo: 'home' },
 ];
