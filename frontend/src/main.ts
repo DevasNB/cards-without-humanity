83016779c8f10d65a1a4461bb39ef8dd228edde2
João Sá <joaofilipesa2020@gmail.com>
2025-12-21 06:23:39 +0000

    fix!: probably now everything is fixed - bye workspaces

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-// TODO: "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
